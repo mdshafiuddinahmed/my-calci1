ce9e189f38b8faf139f8680cfcc91da992b2dc79
mdshafiuddinahmed <shafiuddinahmed2001@gmail.com>
2023-01-15 16:56:01 +0530

    First commit

---
+++ public/App.js
@@ -0,0 +1,13 @@
+(function(){
+
+    let screen= document.querySelector('.screen');
+    let buttons= document.querySelector('.btn');
+    let clear= document.querySelector('.btn-clear');
+    let equal= document.querySelector('.btn-equal');
+
+    buttons.foreach(function(buttons)){
+        buttons.addEventListener('click', function(e){
+            let value
+        })
+    }
+})
